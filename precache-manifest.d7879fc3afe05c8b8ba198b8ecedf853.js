a1f3eba01772097b41b16016f0c7b948ff800e7b
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-12-25 07:35:53 +0000

    Deploy BARMPlus/ccz-vikingship to github.com/BARMPlus/ccz-vikingship.git:gh-pages

---
+++ precache-manifest.d7879fc3afe05c8b8ba198b8ecedf853.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ebf9611c63372eb4606347f9a317a15",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.6ac754dc079073d4ef3d.bundle.js"
+  },
+  {
+    "url": "runtime~main.6ac754dc079073d4ef3d.bundle.js"
+  },
+  {
+    "revision": "684c400eb6c43e4bdf74",
+    "url": "static/css/main.031105c5.chunk.css"
+  },
+  {
+    "url": "vendors~main.6ac754dc079073d4ef3d.bundle.js"
+  },
+  {
+    "url": "vendors~main.6ac754dc079073d4ef3d.bundle.js.LICENSE.txt"
+  }
+]);
